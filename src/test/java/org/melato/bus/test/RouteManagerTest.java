3f4e18b7ab4e466f7c664094cefedd7c3ab6b783
Alex Athanasopoulos <alex@melato.org>
2012-08-31 01:05:55 +0300

    Added a test for GPX file

--- src/test/java/org/melato/bus/test/RouteManagerTest.java
+++ src/test/java/org/melato/bus/test/RouteManagerTest.java
@@ -8,6 +8,7 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 import org.melato.bus.model.RouteManager;
+import org.melato.gpx.GPX;
 import org.melato.gpx.Point;
 import org.melato.gpx.Waypoint;
 import org.xml.sax.SAXException;
@@ -19,5 +20,13 @@ public class RouteManagerTest {
     Point target = new Point(37.98581f, 23.739164f);
     List<Waypoint> near = routeManager.findNearbyStops(target, 50);
     Assert.assertEquals(1, near.size());
+  }
+  @Test public void testGPXRoute() throws Exception {
+    URL url = getClass().getResource("data/");
+    RouteManager routeManager = new RouteManager(url);
+    GPX gpx = routeManager.loadGPX("021-1");
+    Assert.assertEquals(1, gpx.getRoutes().size());
+    List<Waypoint> waypoints = gpx.getRoutes().get(0).path.getWaypoints();
+    Assert.assertTrue(waypoints.size() > 2 );
   }
 }
