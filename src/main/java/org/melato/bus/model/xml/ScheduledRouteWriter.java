9c9a27080922e6b1306ae578eec0e26f30547a24
Alex Athanasopoulos <alex@melato.org>
2012-09-23 08:47:20 +0300

    xml formatting

--- src/main/java/org/melato/bus/model/xml/ScheduledRouteWriter.java
+++ src/main/java/org/melato/bus/model/xml/ScheduledRouteWriter.java
@@ -1,5 +1,8 @@
 package org.melato.bus.model.xml;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.melato.bus.model.DaySchedule;
 import org.melato.bus.model.Route;
 import org.melato.bus.model.Schedule;
@@ -17,16 +20,17 @@ public class ScheduledRouteWriter extends RouteWriter {
       return;
     }
     for( DaySchedule daySchedule: schedule.getSchedules() ) {
+      xml.println();
       xml.tagOpen( SCHEDULE, false );
       xml.tagAttribute(DAYS, String.valueOf(daySchedule.getDays()));
       xml.tagClose();
-      xml.println();
       for( int time: daySchedule.getTimes() ) {
+        xml.println();
         xml.tagOpen( TIME, false );
         xml.tagAttribute( VALUE, Schedule.formatTime(time));
         xml.tagClose(true);
-        xml.println();
       }
+      xml.println();
       xml.tagEnd( SCHEDULE );
       xml.println();
     }
@@ -37,5 +41,12 @@ public class ScheduledRouteWriter extends RouteWriter {
     routeBody(route, xml);
     writeSchedules(sr.getSchedule(), xml);    
     routeEnd(route, xml);
+  }
+  public void write(ScheduledRoute sr, File file) throws IOException {
+    XMLWriter xml = new XMLWriter(file);
+    begin(xml);
+    write(sr, xml);
+    end(xml);
+    xml.close();
   }
 }
