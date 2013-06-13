de676e829f5383f40277772e8d777eeae6869703
Alex Athanasopoulos <alex@melato.org>
2013-06-13 19:19:46 +0300

    Added fields to Municipality

--- src/main/java/org/melato/bus/model/Municipality.java
+++ src/main/java/org/melato/bus/model/Municipality.java
@@ -1,18 +1,99 @@
 package org.melato.bus.model;
 
-public class Municipality {
-  private String name;
+import java.io.Serializable;
+
+import org.melato.gps.Point2D;
+
+public class Municipality implements Serializable {
+  public String name;
+  public String mayor;
+  public String police;
+  public String website;
+  public String address;
+  public String postalCode;
+  public String city;
+  public Point2D point;
+
   public String getName() {
-    return name;    
+    return name;
   }
+  
+  public String getMayor() {
+    return mayor;
+  }
+
+
+  public void setMayor(String mayor) {
+    this.mayor = mayor;
+  }
+
+
+  public String getPolice() {
+    return police;
+  }
+
+
+  public void setPolice(String police) {
+    this.police = police;
+  }
+
+
+  public String getWebsite() {
+    return website;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getPostalCode() {
+    return postalCode;
+  }
+
+  public void setPostalCode(String postalCode) {
+    this.postalCode = postalCode;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public Point2D getPoint() {
+    return point;
+  }
+
+  public void setPoint(Point2D point) {
+    this.point = point;
+  }
+
+  public void setWebsite(String website) {
+    this.website = website;
+  }
+
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
   @Override
   public String toString() {
     return name != null ? name : "";
   }
+
   public Municipality(String name) {
     super();
     this.name = name;
   }
+
   @Override
   public int hashCode() {
     final int prime = 31;
@@ -20,6 +101,7 @@ public class Municipality {
     result = prime * result + ((name == null) ? 0 : name.hashCode());
     return result;
   }
+
   @Override
   public boolean equals(Object obj) {
     if (this == obj)
@@ -35,5 +117,5 @@ public class Municipality {
     } else if (!name.equals(other.name))
       return false;
     return true;
-  }    
+  }
 }
