2df9f572deb41ec64ac9da7801a2080596b0ff66
Alex Athanasopoulos <alex@melato.org>
2013-07-03 12:05:53 +0300

    string node instead of int

--- src/main/java/org/melato/bus/client/HelpItem.java
+++ src/main/java/org/melato/bus/client/HelpItem.java
@@ -1,33 +1,38 @@
 package org.melato.bus.client;
 
+/** A help item.  Used to display a page of help information in an android activity. */
 public class HelpItem {
-  String name;
-  String title;
-  String text;
-  int   node;
+  private String name;
+  private String title;
+  private String text;
+  private String node;
     
+  /** The title of the item.  Should be short enough for an activity title.  */ 
   public String getTitle() {
     return title;
   }
   public void setTitle(String title) {
     this.title = title;
   }
+  /** Internal optional mnemonic id for the item. */
   public String getName() {
     return name;
   }
   public void setName(String name) {
     this.name = name;
   }
+  /** The text content (body) of the item.  */
   public String getText() {
     return text;
   }
   public void setText(String text) {
     this.text = text;
   }
-  public int getNode() {
+  /** Internal persistent id for the item. */
+  public String getNode() {
     return node;
   }
-  public void setNode(int node) {
+  public void setNode(String node) {
     this.node = node;
   }
 }
