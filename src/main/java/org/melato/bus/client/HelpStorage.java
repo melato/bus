eed470d9482f241b8434e8504a47eb4fb3a9c695
Alex Athanasopoulos <alex@melato.org>
2013-07-04 11:32:55 +0300

    renamed methods.  Added lang argument.

--- src/main/java/org/melato/bus/client/HelpStorage.java
+++ src/main/java/org/melato/bus/client/HelpStorage.java
@@ -1,6 +1,14 @@
 package org.melato.bus.client;
 
 public interface HelpStorage {
-  HelpItem loadHelp(String name);
-  HelpItem loadHelp(int node);
+  /** Load a help item by name.
+   * @param name
+   * @param lang The language code (e.g. "el", "en").
+   * @return The help item for the specific language, or for the default language.
+   */
+  HelpItem loadHelpByName(String name, String lang);
+  /** Load a help item by internal node code.
+   * @param node The node code.
+   */
+  HelpItem loadHelpByNode(String node);
 }
