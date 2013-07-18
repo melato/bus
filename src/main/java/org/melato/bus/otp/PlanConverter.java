395eff41b11b80dc36fedfcf9cc8e9912fa6d4dd
Alex Athanasopoulos <alex@melato.org>
2013-07-18 22:42:33 +0300

    Convert OTP.Itinerary to Sequence.

---
+++ src/main/java/org/melato/bus/otp/PlanConverter.java
@@ -0,0 +1,101 @@
+package org.melato.bus.otp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.melato.bus.model.Route;
+import org.melato.bus.model.RouteId;
+import org.melato.bus.model.RouteManager;
+import org.melato.bus.model.Stop;
+import org.melato.bus.plan.Leg;
+import org.melato.bus.plan.Plan;
+import org.melato.bus.plan.PlanLeg;
+import org.melato.bus.plan.Sequence;
+import org.melato.gps.Point2D;
+
+/** Converts OTP structures to our model. */
+public class PlanConverter {
+  private RouteManager routeManager;
+  
+  public static class MismatchException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public MismatchException(String message) {
+      super(message);
+    }
+  }
+    
+  public PlanConverter(RouteManager routeManager) {
+    super();
+    this.routeManager = routeManager;
+  }
+
+  public void setRouteManager(RouteManager routeManager) {
+    this.routeManager = routeManager;
+  }
+
+  private Stop findStop(RouteId route, Stop[] stops, int offset, String code) throws MismatchException {
+    for( int i = offset; i < stops.length; i++ ) {
+      Stop stop = stops[i];
+      if ( stop.getSymbol().equals(code)) {
+        return stop;
+      }
+    }
+    throw new MismatchException("cannot find stop: " + route + "(" + offset + ") -" + code + "-");
+  }
+
+  public Leg convertLeg(OTP.TransitLeg t) throws MismatchException {
+    RouteId routeId = new RouteId(t.routeId);
+    if ( routeManager.getRoute(routeId) == null) {
+      throw new MismatchException("Unknown route: " + t.routeId);
+    }
+    Stop[] stops = routeManager.getStops(routeId);
+    Stop stop1 = findStop(routeId, stops, 0, t.from.stopCode);
+    Stop stop2 = findStop(routeId, stops, stop1.getIndex(), t.to.stopCode);
+    return new Leg(routeId, stop1, stop2);    
+  }
+  
+  public Sequence convertToSequence(OTP.Itinerary it) throws MismatchException {
+    Sequence sequence = new Sequence();
+    for( OTP.Leg otpLeg: it.legs) {
+      if ( otpLeg instanceof OTP.TransitLeg ) {
+        OTP.TransitLeg t = (OTP.TransitLeg) otpLeg;
+        sequence.addLeg(convertLeg(t));
+      }
+    }
+    return sequence;    
+  }
+  
+  public Plan convertItinerary(OTP.Itinerary it, Point2D origin, Point2D destination) throws MismatchException {
+    List<PlanLeg> legs = new ArrayList<PlanLeg>();
+    for(OTP.Leg otpLeg: it.legs){
+      if ( otpLeg instanceof OTP.TransitLeg) {
+        OTP.TransitLeg transitLeg = (OTP.TransitLeg)otpLeg;
+        RouteId routeId = new RouteId(transitLeg.routeId);
+        Route route = routeManager.getRoute(routeId);
+        if ( route == null) {
+          throw new MismatchException( "cannot find route: " + transitLeg.routeId);
+        }
+        Stop[] stops = routeManager.getStops(routeId);
+        Stop stop1 = findStop(routeId, stops, 0, transitLeg.from.id);
+        Stop stop2 = findStop(routeId, stops, stop1.getIndex() + 1, transitLeg.to.id);        
+        PlanLeg leg = new PlanLeg(route, stop1, stop2);
+        legs.add(leg);
+      }
+    }
+    return new Plan(origin, destination, legs.toArray(new PlanLeg[0]));
+  }
+  
+  public Plan[] convertPlan(OTP.Plan otp, PlanRequest request) {
+    List<Plan> plans = new ArrayList<Plan>();
+    for(OTP.Itinerary it: otp.itineraries) {
+      try {
+        Plan plan = convertItinerary(it, request.getFromPlace(), request.getToPlace());
+        plans.add(plan);
+      } catch(MismatchException e) {
+        System.out.println(e);
+      }
+    }
+    return plans.toArray(new Plan[0]);
+  }
+}
